3f8e442a32bbf77d45dd822a450e136cebb10356
molpopgen <krthornt@uci.edu>
2016-07-16 16:43:50 -0400

    comment the Cython plugin

--- snowdrift.pyx
+++ snowdrift.pyx
@@ -8,15 +8,23 @@ cimport cython
 cdef extern from "<algorithm>" namespace "std" nogil:
     T max[T](const T &, const T &)
 
+#fwdpy doesn't initialize the 'label' value of a diploid
+#We do it here in case of situations where
+#one also wants to set the population up with a specific mutation.
+#Without this, we wouldn't a correct fitness calculation in the
+#first generation
 cdef inline void label_diploids_cpp(singlepop_t * pop):
     cdef size_t i=0
     for i in range(pop.diploids.size()):
         pop.diploids[i].label=i
-    
+
+#Data type to track stuff about trait values.
+#The doubles are params of the snowdrift model
 cdef cppclass snowdrift_data:
     vector[double] phenotypes
     double b1,b2,c1,c2
 
+#Function to return an initialized snowdrift_data object
 cdef inline snowdrift_data make_snowdrift_data(double b1,double b2, double c1, double c2):
     cdef snowdrift_data d
     d.phenotypes=vector[double]()
@@ -26,6 +34,7 @@ cdef inline snowdrift_data make_snowdrift_data(double b1,double b2, double c1, d
     d.c2=c2
     return d
 
+#The fitness model:
 cdef inline double snowdrift_fitness(const diploid_t & dip,
                                      const gcont_t & gametes,
                                      const mcont_t & mutations,
@@ -38,10 +47,13 @@ cdef inline double snowdrift_fitness(const diploid_t & dip,
         if i != dip.label:
             zpair = zself + d.phenotypes[i]
             a = d.b1*zpair + d.b2*zpair*zpair - d.c1*zself - d.c2*zself*zself
-            fitness += a#(max[double](a,0.0))
+            fitness += a
     fitness/=<double>(d.phenotypes.size()-1)
     return max[double](1+fitness,0.0)
 
+#The updater function:
+#Invidual fitness is additive over size, P = sum_i e_i,
+#where e_i is i-th effect size
 cdef inline void snowdrift_update(const singlepop_t * pop, snowdrift_data & d) nogil:
     if d.phenotypes.size() < pop.diploids.size():
         d.phenotypes.resize(pop.diploids.size())
@@ -55,20 +67,24 @@ cdef inline void snowdrift_update(const singlepop_t * pop, snowdrift_data & d) n
                             <genotype_fitness_updater>het_additive_update,
                             <fitness_function_finalizer>return_trait_value,
                             <double>0.0)
-    
+
+#Nicer name for our stateful fitness type:
 ctypedef singlepop_fitness_data[snowdrift_data] singlepop_fitness_snowdrift
 
 cdef class SpopSnowdrift(SpopFitness):
     """
-    This is the fitness model
+    This is the fitness model as a Cython extension class
     """
     def __cinit__(self,double b1,double b2, double c1, double c2):
         """
-        Constructor takes the params for the model.
+        Constructor takes the params for the payoff functions.
         """
         cdef snowdrift_data d = make_snowdrift_data(b1,b2,c1,c2)
         self.wfxn=<unique_ptr[singlepop_fitness]>unique_ptr[singlepop_fitness_snowdrift](new singlepop_fitness_snowdrift(snowdrift_fitness,
                                                                                                                          snowdrift_update,d))
 
 def label_diploids(Spop pop):
+    """
+    Python fxn to label the diploids
+    """
     label_diploids_cpp(pop.pop.get())
